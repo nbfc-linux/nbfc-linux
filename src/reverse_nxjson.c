606ea67d8c6c74e38da2850b26848b61687c723f
rofl0r <rofl0r@users.noreply.github.com>
2021-11-15 22:15:33 +0000

    reverse_nxjson: remove insane allocation of 10000000000000 bytes
    1e13 is 10000000000000, which doesn't even fit into an int.
    instead, use a stack buffer of 128 kbyte which should be plenty,
    then strdup the actual string on return.
    note that this entire function is horribly inefficient as the
    string has to be scanned over and over.
    an efficient implementation would use
    
        len = snprintf(buf, sizeof buf "[%s]", s)
    
    instead of
    
        strcat(s, "["), strcat(s, t), strcat(s, "]")
    
    , and then add len to the start pointer.
    
    while at it, also rename the confusing IF_... macro which suggests an
    if block starts, and use a function-like macro.

--- src/reverse_nxjson.c
+++ src/reverse_nxjson.c
@@ -3,49 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define IF_KEY_NOT_NULL                                                        \
+#define ADD_KEY_NOT_NULL()                                                     \
   if (nx->key != NULL) {                                                       \
     strcat(s, "\"");                                                           \
     strcat(s, nx->key);                                                        \
     strcat(s, "\": ");                                                         \
   }
 
+
 char *nx_json_to_string(const nx_json *nx) {
-  const int SIZE = (int)1e13;
-  char *s = malloc(SIZE);
-  strcpy(s, "");
+  char s[128*1024];
+  s[0] = 0;
   while (nx != NULL) {
     if (nx->type == NX_JSON_OBJECT) {
-      IF_KEY_NOT_NULL
+      ADD_KEY_NOT_NULL();
       strcat(s, "{");
       strcat(s, nx_json_to_string(nx->val.children.first));
       strcat(s, "}");
     } else if (nx->type == NX_JSON_ARRAY) {
-      IF_KEY_NOT_NULL
+      ADD_KEY_NOT_NULL();
       strcat(s, "[");
       strcat(s, nx_json_to_string(nx->val.children.first));
       strcat(s, "]");
     } else {
       if (nx->type == NX_JSON_STRING) {
-        IF_KEY_NOT_NULL
+        ADD_KEY_NOT_NULL();
         strcat(s, "\"");
         strcat(s, nx->val.text);
         strcat(s, "\"");
       } else if (nx->type == NX_JSON_BOOL) {
-        IF_KEY_NOT_NULL
+        ADD_KEY_NOT_NULL();
         strcat(s, nx->val.u ? "true" : "false");
       } else if (nx->type == NX_JSON_INTEGER) {
-        IF_KEY_NOT_NULL
+        ADD_KEY_NOT_NULL();
         char i[snprintf(NULL, 0, "%ld", nx->val.i) + 1];
         sprintf(i, "%ld", nx->val.i);
         strcat(s, i);
       } else if (nx->type == NX_JSON_DOUBLE) {
-        IF_KEY_NOT_NULL
+        ADD_KEY_NOT_NULL();
         char dbl[snprintf(NULL, 0, "%lf", nx->val.dbl) + 1];
         sprintf(dbl, "%lf", nx->val.dbl);
         strcat(s, dbl);
       } else {
-        IF_KEY_NOT_NULL
+        ADD_KEY_NOT_NULL();
         strcat(s, "null");
       }
     }
@@ -53,5 +53,5 @@ char *nx_json_to_string(const nx_json *nx) {
     if (nx != NULL)
       strcat(s, ", ");
   }
-  return s;
+  return strdup(s);
 }
