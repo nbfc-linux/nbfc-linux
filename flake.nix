5182dc0d4e66a67f976b291e122e007000ccc720
natto1784 <singh.amneesh1784@gmail.com>
2022-03-09 14:21:20 +0530

    flake.nix: update stuff
    add separate packages for both clients and clean up things

--- flake.nix
+++ flake.nix
@@ -1,57 +1,47 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = github:nixOS/nixpkgs;
     utils.url = github:numtide/flake-utils;
   };
   outputs = { nixpkgs, utils, ... }:
-  utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ] 
-  ( system:
-  let 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-  in 
-  {
-    defaultPackage = pkgs.stdenv.mkDerivation {
-      name = "nbfc-linux";
-      src = ./.;
-      buildInputs = with pkgs; [
-        python3
-      ];
+    utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          lib = nixpkgs.lib;
 
-    buildPhase = ''
-      make sharedir=$out/share -C src
-    '';
+          buildPackage = { client ? "py" }:
+            assert lib.assertMsg (builtins.elem client [ "py" "python" "c" ]) "Client implementation is only available in Python and C";
 
-    installPhase = ''
-      mkdir -p $out/bin
-      install nbfc.py           $out/bin/nbfc
-      install src/nbfc_service  $out/bin/nbfc_service
-      install src/ec_probe      $out/bin/ec_probe
-  #   install src/nbfc          $out/bin/nbfc   #client written in c   
-      mkdir -p $out/share/nbfc/configs
-      cp -r share/nbfc/configs/* $out/share/nbfc/configs
-      
-      mkdir -p $out/share/man/man1
-      mkdir -p $out/share/man/man5
-      cp doc/ec_probe.1            $out/share/man/man1
-      cp doc/nbfc.1                $out/share/man/man1
-      cp doc/nbfc_service.1        $out/share/man/man1
-      cp doc/nbfc_service.json.5   $out/share/man/man5
-      
-      mkdir -p $out/share/zsh/site-functions
-      cp completion/zsh/_nbfc                $out/share/zsh/site-functions/
-      cp completion/zsh/_nbfc_service        $out/share/zsh/site-functions/
-      cp completion/zsh/_ec_probe            $out/share/zsh/site-functions/
-      mkdir -p $out/share/bash-completion/completions
-      cp completion/bash/nbfc                $out/share/bash-completion/completions/
-      cp completion/bash/nbfc_service        $out/share/bash-completion/completions/
-      cp completion/bash/ec_probe            $out/share/bash-completion/completions/
-      mkdir -p $out/share/fish/completions
-      cp completion/fish/nbfc.fish           $out/share/fish/completions/
-      cp completion/fish/nbfc_service.fish   $out/share/fish/completions/
-      cp completion/fish/ec_probe.fish       $out/share/fish/completions/
-    '';
-    };
-  });
+            pkgs.stdenv.mkDerivation {
+              name = "nbfc-linux";
+              version = "0.1.6";
+              src = lib.cleanSource ./.;
+
+              buildInputs = lib.optional (builtins.elem client [ "py" "python" ]) pkgs.python3;
+              buildFlags = [ "PREFIX=$(out)" "confdir=/etc" ];
+
+              installPhase =
+                let
+                  installFlags = [ "PREFIX=$out" ];
+                in
+                ''
+                  make ${builtins.concatStringsSep " " installFlags}\
+                       install-core \
+                       install-client-${if (client == "c") then "c" else "py" }\
+                       install-configs\
+                       install-docs\
+                       install-completion
+                '';
+            };
+        in
+        rec {
+          packages = rec {
+            nbfc = buildPackage { client = "py"; };
+            nbfc-client-c = buildPackage { client = "c"; };
+          };
+          defaultPackage = packages.nbfc;
+        });
 }
