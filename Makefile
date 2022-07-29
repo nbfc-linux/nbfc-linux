79796b975b79a29609042970b31fa6fb8bd257d9
allaeddine boulefaat <allaeddineboulefaat@gmail.com>
2022-07-29 17:09:47 +0100

    initial appimage support

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-PREFIX   = /usr
+PREFIX   = /
 
 bindir   = $(PREFIX)/bin
 confdir  = $(PREFIX)/etc
--- build-appimage.sh
+++ build-appimage.sh
@@ -8,13 +8,17 @@ if [ ! -e "appimagetool-x86_64.AppImage" ] ; then
     chmod +x "appimagetool-x86_64.AppImage"
 fi
 
-wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64"
-mv "AppRun-x86_64" "AppDir/AppRun"
-chmod +x "AppDir/AppRun"
+#wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64"
+#mv "AppRun-x86_64" "AppDir/AppRun"
+#chmod +x "AppDir/AppRun"
 
 cp "data/nbfc-linux.svg" "AppDir/nbfc-linux.svg"
 cp "data/nbfc-linux.desktop" "AppDir/nbfc-linux.desktop"
-cp "data/run" "AppDir/usr/bin/run"
-chmod +x "AppDir/usr/bin/run"
+#cp "data/run" "AppDir/usr/bin/run"
+#chmod +x "AppDir/usr/bin/run"
+
+cp "data/run" "AppDir/AppRun"
+chmod +x "AppDir/AppRun"
 
 ./appimagetool-x86_64.AppImage AppDir
+chmod +x nbfc-linux-*.AppImage
