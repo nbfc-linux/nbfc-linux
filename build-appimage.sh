58acaa364b99705cc399a3b258b335296aa46a17
allaeddine boulefaat <allaeddineboulefaat@gmail.com>
2022-07-29 15:00:27 +0100

    initial appimage support

--- build-appimage.sh
+++ build-appimage.sh
@@ -7,9 +7,10 @@ if [ ! -e "appimagetool-x86_64.AppImage" ] ; then
     wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" "appimagetool-x86_64.AppImage"
 fi
 
-wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64" "Appdir/AppRun"
+wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64" "AppDir/AppRun"
+chmod +x "AppDir/AppRun"
 
-cp "data/nbfc-linux.svg" "Appdir/nbfc-linux.svg"
-cp "data/nbfc-linux.desktop" "Appdir/nbfc-linux.desktop"
+cp "data/nbfc-linux.svg" "AppDir/nbfc-linux.svg"
+cp "data/nbfc-linux.desktop" "AppDir/nbfc-linux.desktop"
 
-./appimagetool-x86_64.AppImage AppDIr
+./appimagetool-x86_64.AppImage AppDir
