568e63105414ad292fd770ab57a0336788a17fff
allaeddine boulefaat <allaeddineboulefaat@gmail.com>
2022-07-29 15:04:01 +0100

    initial appimage support

--- build-appimage.sh
+++ build-appimage.sh
@@ -5,6 +5,7 @@ DESTDIR=AppDir make install
 
 if [ ! -e "appimagetool-x86_64.AppImage" ] ; then
     wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" "appimagetool-x86_64.AppImage"
+    chmod +x "appimagetool-x86_64.AppImage"
 fi
 
 wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64" "AppDir/AppRun"
