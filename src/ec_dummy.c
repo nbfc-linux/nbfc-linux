15416711d46ca0c9f7f1342bd509f61c8248d468
Benjamin Abendroth <braph93@gmx.de>
2024-01-17 09:26:03 +0100

    Changes in ec_dummy.c

--- src/ec_dummy.c
+++ src/ec_dummy.c
@@ -1,31 +1,21 @@
 #include "ec_dummy.h"
 
+#include <endian.h>
+
 #include "memory.h"
 
-static uint16_t* EC_Dummy_FakeRegisters = NULL;
-#define          EC_Dummy_FakeRegistersSize 256
+static uint8_t* EC_Dummy_FakeRegisters = NULL;
+#define         EC_Dummy_FakeRegistersSize 256
 
 Error* EC_Dummy_Open() {
   if (! EC_Dummy_FakeRegisters)
-    EC_Dummy_FakeRegisters = (uint16_t*) Mem_Calloc(EC_Dummy_FakeRegistersSize, sizeof(uint16_t));
+    EC_Dummy_FakeRegisters = (uint8_t*) Mem_Calloc(EC_Dummy_FakeRegistersSize, sizeof(uint8_t));
   return err_success();
 }
 
 void EC_Dummy_Close() {
   Mem_Free(EC_Dummy_FakeRegisters);
   EC_Dummy_FakeRegisters = NULL;
-}
-
-Error* EC_Dummy_WriteByte(int register_, uint8_t value) {
-  if (register_ < EC_Dummy_FakeRegistersSize)
-    EC_Dummy_FakeRegisters[register_] = value;
-  return err_success();
-}
-
-Error* EC_Dummy_WriteWord(int register_, uint16_t value) {
-  if (register_ < EC_Dummy_FakeRegistersSize)
-    EC_Dummy_FakeRegisters[register_] = value;
-  return err_success();
 }
 
 Error* EC_Dummy_ReadByte(int register_, uint8_t* out) {
@@ -35,10 +25,30 @@ Error* EC_Dummy_ReadByte(int register_, uint8_t* out) {
   return err_success();
 }
 
-Error* EC_Dummy_ReadWord(int register_, uint16_t* out) {
-  *out = 0;
+Error* EC_Dummy_WriteByte(int register_, uint8_t value) {
   if (register_ < EC_Dummy_FakeRegistersSize)
-    *out = EC_Dummy_FakeRegisters[register_];
+    EC_Dummy_FakeRegisters[register_] = value;
+  return err_success();
+}
+
+Error* EC_Dummy_ReadWord(int register_, uint16_t* out) {
+  if (register_ + 1 < EC_Dummy_FakeRegistersSize) {
+    *out = ((uint16_t) EC_Dummy_FakeRegisters[register_]) |
+          (((uint16_t) EC_Dummy_FakeRegisters[register_+1]) << 8);
+  }
+  return err_success();
+}
+
+Error* EC_Dummy_WriteWord(int register_, uint16_t value) {
+  value = htole16(value);
+
+  uint8_t msb = value >> 8;
+  uint8_t lsb = value;
+
+  if (register_ + 1 < EC_Dummy_FakeRegistersSize) {
+    EC_Dummy_FakeRegisters[register_] = lsb;
+    EC_Dummy_FakeRegisters[register_ + 1] = msb;
+  }
   return err_success();
 }
 
