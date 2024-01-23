1aaa77a5ed13ef6c5d18096e73a44ca3d887f5ab
Benjamin Abendroth <braph93@gmx.de>
2024-01-23 16:47:07 +0100

    Updated integrated help texts of binaries

--- src/generated/ec_probe.help.h
+++ src/generated/ec_probe.help.h
@@ -1,69 +1,76 @@
-#define EC_PROBE_HELP_TEXT "usage: %s [-h] [-e EC] {dump,read,write,monitor} ...\n"\
- "\n"\
- "Probing tool for embedded controllers\n"\
- "\n"\
- "optional arguments:\n"\
- "  -h, --help            show this help message and exit\n"\
- "  -e EC, --embedded-controller EC\n"\
- "                        Specify embedded controller to use\n"\
- "\n"\
- "subcommands:\n"\
- "  commands\n"\
- "\n"\
- "  {dump,read,write,monitor}\n"\
- "    dump                Dump all EC registers\n"\
- "    read                Read a byte from a EC register\n"\
- "    write               Write a byte to a EC register\n"\
- "    monitor             Monitor all EC registers for changes\n"\
- "\n"\
+#define EC_PROBE_HELP_TEXT                                                     \
+ "Usage: %s [-h] [-e EC] COMMAND [...]\n"                                      \
+ "\n"                                                                          \
+ "Probing tool for embedded controllers\n"                                     \
+ "\n"                                                                          \
+ "Optional arguments:\n"                                                       \
+ "  -h, --help            Show this help message and exit\n"                   \
+ "  -e EC, --embedded-controller EC\n"                                         \
+ "                        Specify embedded controller to use\n"                \
+ "\n"                                                                          \
+ "Commands:\n"                                                                 \
+ "    dump                Dump all EC registers\n"                             \
+ "    read                Read a byte from a EC register\n"                    \
+ "    write               Write a byte to a EC register\n"                     \
+ "    monitor             Monitor all EC registers for changes\n"              \
+ "\n"                                                                          \
  "All input values are interpreted as decimal numbers by default. Hexadecimal values may be entered by prefixing them with \"0x\".\n"\
  ""
-#define EC_PROBE_DUMP_HELP_TEXT "usage: %s [-h]\n"\
- "\n"\
- "Dump all EC registers\n"\
- "\n"\
- "optional arguments:\n"\
- "  -h, --help  show this help message and exit\n"\
+
+#define EC_PROBE_DUMP_HELP_TEXT                                                \
+ "Usage: %s dump [-h]\n"                                                       \
+ "\n"                                                                          \
+ "Dump all EC registers\n"                                                     \
+ "\n"                                                                          \
+ "Optional arguments:\n"                                                       \
+ "  -h, --help  Show this help message and exit\n"                             \
  ""
 
-#define EC_PROBE_READ_HELP_TEXT "usage: %s [-h] REGISTER\n"\
- "\n"\
- "Read a byte from a EC register\n"\
- "\n"\
- "positional arguments:\n"\
- "  REGISTER    Register source\n"\
- "\n"\
- "optional arguments:\n"\
- "  -h, --help  show this help message and exit\n"\
- "  -w, --word  read two registers as one word\n"\
+#define EC_PROBE_READ_HELP_TEXT                                                \
+ "Usage: %s read [-h] REGISTER\n"                                              \
+ "\n"                                                                          \
+ "Read a byte from a EC register\n"                                            \
+ "\n"                                                                          \
+ "Positional arguments:\n"                                                     \
+ "  REGISTER    Register source\n"                                             \
+ "\n"                                                                          \
+ "Optional arguments:\n"                                                       \
+ "  -h, --help  Show this help message and exit\n"                             \
+ "  -w, --word  Read two registers as one word\n"                              \
+ "\n"                                                                          \
+ "All input values are interpreted as decimal numbers by default. Hexadecimal values may be entered by prefixing them with \"0x\".\n"\
  ""
 
-#define EC_PROBE_WRITE_HELP_TEXT "usage: %s [-h] REGISTER VALUE\n"\
- "\n"\
- "Write a byte to a EC register\n"\
- "\n"\
- "positional arguments:\n"\
- "  REGISTER    Register destination\n"\
- "  VALUE       Value to write\n"\
- "\n"\
- "optional arguments:\n"\
- "  -h, --help  show this help message and exit\n"\
- "  -w, --word  write VALUE to two registers\n"\
+#define EC_PROBE_WRITE_HELP_TEXT                                               \
+ "Usage: %s write [-h] REGISTER VALUE\n"                                       \
+ "\n"                                                                          \
+ "Write a byte to a EC register\n"                                             \
+ "\n"                                                                          \
+ "Positional arguments:\n"                                                     \
+ "  REGISTER    Register destination\n"                                        \
+ "  VALUE       Value to write\n"                                              \
+ "\n"                                                                          \
+ "Optional arguments:\n"                                                       \
+ "  -h, --help  Show this help message and exit\n"                             \
+ "  -w, --word  Write VALUE to two registers\n"                                \
+ "\n"                                                                          \
+ "All input values are interpreted as decimal numbers by default. Hexadecimal values may be entered by prefixing them with \"0x\".\n"\
  ""
 
-#define EC_PROBE_MONITOR_HELP_TEXT "usage: %s [-h] [-i seconds] [-t seconds] [-r REPORT] [-c] [-d]\n"\
- "\n"\
- "Monitor all EC registers for changes\n"\
- "\n"\
- "optional arguments:\n"\
- "  -h, --help            show this help message and exit\n"\
- "  -i seconds, --interval seconds\n"\
- "                        Monitored timespan\n"\
- "  -t seconds, --timespan seconds\n"\
- "                        Set poll intervall\n"\
- "  -r REPORT, --report REPORT\n"\
- "                        Save all readings as a CSV file\n"\
- "  -c, --clearly         Blanks out consecutive duplicate readings\n"\
+#define EC_PROBE_MONITOR_HELP_TEXT                                             \
+ "Usage: %s monitor [-h] [-i seconds] [-t seconds] [-r FILE] [-c] [-d]\n"      \
+ "\n"                                                                          \
+ "Monitor all EC registers for changes\n"                                      \
+ "\n"                                                                          \
+ "Optional arguments:\n"                                                       \
+ "  -h, --help            Show this help message and exit\n"                   \
+ "  -i seconds, --interval SECONDS\n"                                          \
+ "                        Sets the update interval in seconds\n"               \
+ "  -t seconds, --timespan SECONDS\n"                                          \
+ "                        Sets how many seconds the program will run\n"        \
+ "  -r FILE, --report FILE\n"                                                  \
+ "                        Save all readings as a CSV file\n"                   \
+ "  -c, --clearly         Blanks out consecutive duplicate readings\n"         \
  "  -d, --decimal         Output readings in decimal format instead of hexadecimal format\n"\
  ""
 
--- src/generated/nbfc_service.help.h
+++ src/generated/nbfc_service.help.h
@@ -1,16 +1,17 @@
-#define NBFC_SERVICE_HELP_TEXT "usage: %s [-h] [-r] [-f] [-d] [-c config] [-s state.json] [-e EC]\n"\
- "\n"\
- "NoteBook FanControl service\n"\
- "\n"\
- "optional arguments:\n"\
- "  -h, --help            show this help message and exit\n"\
- "  -r, --read-only        Start in read-only mode\n"\
+#define NBFC_SERVICE_HELP_TEXT                                                 \
+ "Usage: %s [-h] [-r] [-f] [-d] [-c config] [-s state.json] [-e EC]\n"         \
+ "\n"                                                                          \
+ "NoteBook FanControl service\n"                                               \
+ "\n"                                                                          \
+ "Optional arguments:\n"                                                       \
+ "  -h, --help            Show this help message and exit\n"                   \
+ "  -r, --read-only       Start in read-only mode\n"                           \
  "  -f, --fork            Switch process to background after sucessfully started\n"\
  "  -d, --debug           Enable tracing of reads and writes of the embedded controller\n"\
- "  -c config, --config-file config\n"\
- "                        Use alternative config file (default /etc/nbfc/nbfc.json)\n"\
- "  -s state.json, --state-file state.json\n"\
- "                        Write state to an alternative file (default /var/run/nbfc_service.state.json)\n"\
- "  -e EC, --embedded-controller EC\n"\
- "                        Specify embedded controller to use\n"\
+ "  -c CONFIG, --config-file CONFIG\n"                                         \
+ "                        Use alternative config file (default " CONFDIR "/nbfc/nbfc.json)\n"\
+ "  -s STATE.JSON, --state-file STATE.JSON\n"                                  \
+ "                        Write state to an alternative file (default " RUNSTATEDIR "/nbfc_service.state.json)\n"\
+ "  -e EC, --embedded-controller EC\n"                                         \
+ "                        Specify embedded controller to use\n"                \
  ""
