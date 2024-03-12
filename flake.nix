d9004050516f9afe40a90ac2d463fa6c07efbc56
Celibistrial <ryan80222@gmail.com>
2024-03-12 10:26:23 +0530

    Added config for HP Pavilion dk15
    Fix breaking change  7feb526
    
    Added declarative NixOS install guide and refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -1,43 +1,34 @@
 {
   inputs = {
-    nixpkgs.url = github:nixOS/nixpkgs/release-23.05;
-    utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixOS/nixpkgs/release-23.05";
+    utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ]
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        rec {
-          packages =
-            let
-              buildFlags = [ "PREFIX=$(out)" "confdir=/etc" ];
-            in
-            rec {
-              nbfc = pkgs.stdenv.mkDerivation {
-                inherit buildFlags;
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachSystem ["i686-linux" "x86_64-linux"]
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages = {
+        nbfc = pkgs.stdenv.mkDerivation {
+          name = "nbfc-linux";
+          version = "0.1.15";
 
-                name = "nbfc-linux";
-                version = "0.1.6";
+          src = nixpkgs.lib.cleanSource ./.;
 
-                src = nixpkgs.lib.cleanSource ./.;
-
-                installPhase =
-                  let
-                    installFlags = [ "PREFIX=$out" ];
-                  in
-                  ''
-                    make ${builtins.concatStringsSep " " installFlags}\
-                         install-core \
-                         install-configs\
-                         install-docs\
-                         install-completion
-                  '';
-              };
-            };
-          defaultPackage = packages.nbfc;
-        });
+          nativeBuildInputs = with pkgs; [
+            autoreconfHook
+          ];
+          configureFlags = [
+            "--prefix=${placeholder "out"}"
+            "--sysconfdir=${placeholder "out"}/etc"
+            "--bindir=${placeholder "out"}/bin"
+          ];
+        };
+        default = packages.nbfc;
+      };
+    });
 }
