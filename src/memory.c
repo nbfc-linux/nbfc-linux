67485e6815f0b5172405081eae0057430a7beffd
Benjamin Abendroth <braph93@gmx.de>
2024-06-26 15:32:58 +0200

    Dropped `const char* cause` argument form Mem_FatalError

--- src/memory.c
+++ src/memory.c
@@ -3,41 +3,40 @@
 #include "macros.h" // unlikely
 #include "nbfc.h"   // NBFC_EXIT_FATAL
 
-#include <errno.h>  // errno
 #include <stdlib.h> // malloc, calloc, realloc, free, exit
 #include <string.h> // strerror
 #include <stdio.h>  // fprintf
 
-static void Mem_FatalError(const char* cause) {
-  fprintf(stderr, "%s: %s\n", cause, strerror(errno));
+static void Mem_FatalError() {
+  fprintf(stderr, "FATAL ERROR: %s\n", strerror(ENOMEM));
   exit(NBFC_EXIT_FATAL);
 }
 
 void* Mem_Malloc(const size_t size) {
   void* p = malloc(size);
   if (unlikely(!p))
-    Mem_FatalError("Mem_Malloc");
+    Mem_FatalError();
   return p;
 }
 
 void* Mem_Calloc(const size_t nmemb, const size_t size) {
   void* p = calloc(nmemb, size);
   if (unlikely(!p))
-    Mem_FatalError("Mem_Calloc");
+    Mem_FatalError();
   return p;
 }
 
 void* Mem_Realloc(void* p, const size_t size) {
   void* new_p = realloc(p, size);
   if (unlikely(! new_p))
-    Mem_FatalError("Mem_Realloc");
+    Mem_FatalError();
   return new_p;
 }
 
 char* Mem_Strdup(const char* s) {
   char* p = strdup(s);
   if (unlikely(!p))
-    Mem_FatalError("Mem_Strdup");
+    Mem_FatalError();
   return p;
 }
 
