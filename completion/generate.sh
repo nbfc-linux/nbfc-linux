40a624f2fa7efe29ff57b5630b5e261f23726bf7
Benjamin Abendroth <braph93@gmx.de>
2024-09-16 17:48:37 +0200

    Updated autocompletion input files

--- completion/generate.sh
+++ completion/generate.sh
@@ -2,14 +2,14 @@
 
 argparse_shell_complete=argparse-shell-complete
 
-$argparse_shell_complete bash ec_probe.py      > bash/ec_probe.in
-$argparse_shell_complete bash nbfc.py          > bash/nbfc.in
-$argparse_shell_complete bash nbfc_service.py  > bash/nbfc_service.in
+$argparse_shell_complete bash ec_probe.yaml      > bash/ec_probe.in
+$argparse_shell_complete bash nbfc.yaml          > bash/nbfc.in
+$argparse_shell_complete bash nbfc_service.yaml  > bash/nbfc_service.in
 
-$argparse_shell_complete fish ec_probe.py      > fish/ec_probe.fish.in
-$argparse_shell_complete fish nbfc.py          > fish/nbfc.fish.in
-$argparse_shell_complete fish nbfc_service.py  > fish/nbfc_service.fish.in
+$argparse_shell_complete fish ec_probe.yaml      > fish/ec_probe.fish.in
+$argparse_shell_complete fish nbfc.yaml          > fish/nbfc.fish.in
+$argparse_shell_complete fish nbfc_service.yaml  > fish/nbfc_service.fish.in
 
-$argparse_shell_complete zsh ec_probe.py       > zsh/_ec_probe.in
-$argparse_shell_complete zsh nbfc.py           > zsh/_nbfc.in
-$argparse_shell_complete zsh nbfc_service.py   > zsh/_nbfc_service.in
+$argparse_shell_complete zsh ec_probe.yaml       > zsh/_ec_probe.in
+$argparse_shell_complete zsh nbfc.yaml           > zsh/_nbfc.in
+$argparse_shell_complete zsh nbfc_service.yaml   > zsh/_nbfc_service.in
