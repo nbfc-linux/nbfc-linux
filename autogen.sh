6977e7afe26133009da917c58c77dc2af775c9c7
Benjamin Abendroth <braph93@gmx.de>
2025-04-09 15:46:24 +0200

    autogen.sh: Use `type` instead of `which`

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-which autoreconf &>/dev/null || {
-  echo "autoreconf not found. please install the 'autoconf' package"
+
+type autoreconf >/dev/null || {
+  echo "autoreconf not found. please install the 'autoconf' package" >&2
   exit 1
 }
+
 autoreconf --force --install
