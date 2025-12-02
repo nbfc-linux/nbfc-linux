55e2da14a1672c5bb1f210de86056b81e6598343
Benjamin Abendroth <braph93@gmx.de>
2025-12-02 18:14:26 +0100

    Updated configuration files
    - Added "HP Pavilion Gaming Laptop 15-ec2xxx.{xml,json}"
    - Added xml/fetch_from_orignal_nbfc.sh
    - Added xml/transform_to_json.sh
    - Removed xml/Makefile

---
+++ xml/transform_to_json.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OUTDIR='../share/nbfc/configs'
+
+IGNORE=(
+	'HP Victus 16-e0175AX.xml'
+	'HP Victus 16-e0xxx.xml'
+)
+
+is_ignored_file() {
+	local filename="$(basename "$1")" ignored_file
+
+	for ignored_file in "${IGNORE[@]}"; do
+		[[ "$filename" == "$ignored_file" ]] && return 0
+	done
+
+	return 1
+}
+
+for file in *.xml unmerged/*.xml; do
+	if is_ignored_file "$file"; then
+		echo "Skipping $file ..."
+	else
+		echo "Converting $file ..."
+		../tools/config_to_json.py --out-dir "$OUTDIR" "$file"
+	fi
+done
