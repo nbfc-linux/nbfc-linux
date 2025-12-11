3b67a71dc9d72c096e0d8593895fbde5fee4362b
Benjamin Abendroth <braph93@gmx.de>
2025-12-11 17:52:12 +0100

    trace.c: Optimized structure

--- src/trace.h
+++ src/trace.h
@@ -1,12 +1,10 @@
 #ifndef TRACE_H_
 #define TRACE_H_
 
-#include <stddef.h>
-
 struct Trace {
   char buf[4096];
-  size_t stack[32];
-  size_t stack_size;
+  unsigned int stack[32];
+  unsigned int stack_size;
 };
 typedef struct Trace Trace;
 
