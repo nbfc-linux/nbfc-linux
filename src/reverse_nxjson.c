6b41ba23e2eb8e649e5e93e7f0522d20509427e7
Benjamin Abendroth <braph93@gmx.de>
2025-12-21 10:32:08 +0100

    reverse_nxjson.c: Refactored (saving 100 bytes)

--- src/reverse_nxjson.c
+++ src/reverse_nxjson.c
@@ -5,61 +5,68 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ADD_KEY_NOT_NULL()                                                     \
+#define StringBuf_ADD_INDENTION(S, N) \
+    StringBuf_Printf(S, "\n%*s", N, "")
+
+static const char* Json_EscapeString(char*, const size_t, const char*);
+
+static void StringBuf_ADD_KEY_NOT_NULL(StringBuf* s, const nx_json* nx) {
   if (nx->key != NULL) {                                                       \
     StringBuf_AddCh(s, '"');                                                   \
     StringBuf_Printf(s, "%s", nx->key);                                        \
     StringBuf_Printf(s, "%s", "\": ");                                         \
   }
-
-#define ADD_INDENTION(N) \
-    StringBuf_Printf(s, "\n%*s", N, "")
-
-static const char* Json_EscapeString(char*, const size_t, const char*);
+}
 
 char *nx_json_to_string(const nx_json *nx, StringBuf* s, int indent) {
-  static char buf[NBFC_MAX_FILE_SIZE];
+  char buf[NBFC_MAX_FILE_SIZE];
 
   while (nx != NULL) {
-    if (nx->type == NX_JSON_OBJECT) {
-      ADD_INDENTION(indent);
-      ADD_KEY_NOT_NULL();
+    switch (nx->type) {
+    case NX_JSON_OBJECT:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
       StringBuf_AddCh(s, '{');
       nx_json_to_string(nx->val.children.first, s, indent + 3);
-      ADD_INDENTION(indent);
+      StringBuf_ADD_INDENTION(s, indent);
       StringBuf_AddCh(s, '}');
-    } else if (nx->type == NX_JSON_ARRAY) {
-      ADD_INDENTION(indent);
-      ADD_KEY_NOT_NULL();
+      break;
+    case NX_JSON_ARRAY:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
       StringBuf_AddCh(s, '[');
       nx_json_to_string(nx->val.children.first, s, indent + 3);
-      ADD_INDENTION(indent);
+      StringBuf_ADD_INDENTION(s, indent);
       StringBuf_AddCh(s, ']');
-    } else {
-      if (nx->type == NX_JSON_STRING) {
-        ADD_INDENTION(indent);
-        ADD_KEY_NOT_NULL();
-        StringBuf_AddCh(s, '"');
-        Json_EscapeString(buf, sizeof(buf), nx->val.text);
-        StringBuf_Printf(s, "%s", buf);
-        StringBuf_AddCh(s, '"');
-      } else if (nx->type == NX_JSON_BOOL) {
-        ADD_INDENTION(indent);
-        ADD_KEY_NOT_NULL();
-        StringBuf_Printf(s, "%s", nx->val.u ? "true" : "false");
-      } else if (nx->type == NX_JSON_INTEGER) {
-        ADD_INDENTION(indent);
-        ADD_KEY_NOT_NULL();
-        StringBuf_Printf(s, "%ld", nx->val.i);
-      } else if (nx->type == NX_JSON_DOUBLE) {
-        ADD_INDENTION(indent);
-        ADD_KEY_NOT_NULL();
-        StringBuf_Printf(s, "%lf", nx->val.dbl);
-      } else if (nx->type == NX_JSON_NULL) {
-        ADD_INDENTION(indent);
-        ADD_KEY_NOT_NULL();
-        StringBuf_Printf(s, "%s", "null");
-      }
+      break;
+    case NX_JSON_STRING:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
+      StringBuf_AddCh(s, '"');
+      Json_EscapeString(buf, sizeof(buf), nx->val.text);
+      StringBuf_Printf(s, "%s", buf);
+      StringBuf_AddCh(s, '"');
+      break;
+    case NX_JSON_BOOL:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
+      StringBuf_Printf(s, "%s", nx->val.u ? "true" : "false");
+      break;
+    case NX_JSON_INTEGER:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
+      StringBuf_Printf(s, "%ld", nx->val.i);
+      break;
+    case NX_JSON_DOUBLE:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
+      StringBuf_Printf(s, "%lf", nx->val.dbl);
+      break;
+    case NX_JSON_NULL:
+      StringBuf_ADD_INDENTION(s, indent);
+      StringBuf_ADD_KEY_NOT_NULL(s, nx);
+      StringBuf_Printf(s, "%s", "null");
+      break;
     }
     nx = nx->next;
     if (nx != NULL)
