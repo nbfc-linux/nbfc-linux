e3e99a521082e1b802d78e7adb1eec8baee4db12
Benjamin Abendroth <braph93@gmx.de>
2026-01-03 21:12:16 +0100

    Harden parse_number/parse_unumber

--- src/parse_unumber.h
+++ src/parse_unumber.h
@@ -6,15 +6,49 @@
 #include <string.h>
 #include <inttypes.h>
 
+/*
+ * Parses an unsigned integer value from a string with strict and predictable
+ * semantics.
+ *
+ * The function accepty only:
+ *  - Positive decimal numbers (base 10)
+ *  - Positive hexadecimal numbers (base 16) with an explicit "0x" or "0X" prefix.
+ *
+ * Hexadecimal parsing is performed explicitly instead of relying on
+ * base-0 conversion. This is intentional to avoid accepting octal values
+ * such as "0777".
+ *
+ * Octal numbers are rarely used and can easily lead to subtle bugs or security
+ * issues when users are unaware that leading zeros imply octal interpretation.
+ * This also protects against cases where a user forgets the 'x' in "0x".
+ *
+ * Unlike strtoull(), negative values are explicitly rejected and treated
+ * as an error; they are not converted or wrapped into unsigned values.
+ */
 static uint64_t parse_unumber(const char* s, uint64_t min, uint64_t max, const char** errmsg) {
+  int base = 10;
+  int start = 0;
+  int is_negative = 0;
+
+  if (s[0] == '-')
+    is_negative = 1;
+
+  if (s[0] == '-' || s[0] == '+')
+    ++start;
+
+  if (s[start] == '0' && (s[start+1] == 'x' || s[start+1] == 'X'))
+    base = 16;
+
   errno = 0;
   char* end;
-  uint64_t val = strtoull(s, &end, 0);
+  uint64_t val = strtoull(s, &end, base);
 
   if (errno)
     *errmsg = strerror(errno);
   else if (!*s || *end)
     *errmsg = strerror(EINVAL);
+  else if (is_negative && val)
+    *errmsg = "value is negative";
   else if (val < min)
     *errmsg = "value too small";
   else if (val > max)
