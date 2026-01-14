7f14dda9cf4e2b85018a6e561e73aec0ffb2a9a0
Benjamin Abendroth <braph93@gmx.de>
2026-01-14 14:32:32 +0100

    ec_debug.c: Only debug if reads/writes are successful, else print warning

--- src/ec_debug.c
+++ src/ec_debug.c
@@ -14,29 +14,45 @@ void EC_Debug_Close() {
 
 Error EC_Debug_WriteByte(uint8_t register_, uint8_t value) {
   Error e = EC_Debug_Controller->WriteByte(register_, value);
-  Log_Debug("WriteByte(0x%X, 0x%X)", register_, value);
-  e_warn();
+
+  if (e)
+    Log_Warn("%s", err_print_all(e));
+  else
+    Log_Debug("WriteByte(0x%X, 0x%X)", register_, value);
+
   return e;
 }
 
 Error EC_Debug_WriteWord(uint8_t register_, uint16_t value) {
   Error e = EC_Debug_Controller->WriteWord(register_, value);
-  Log_Debug("WriteWord(0x%X, 0x%X)", register_, value);
-  e_warn();
+
+  if (e)
+    Log_Warn("%s", err_print_all(e));
+  else
+    Log_Debug("WriteWord(0x%X, 0x%X)", register_, value);
+
   return e;
 }
 
 Error EC_Debug_ReadByte(uint8_t register_, uint8_t* out) {
   Error e = EC_Debug_Controller->ReadByte(register_, out);
-  Log_Debug("ReadByte(0x%X, out = 0x%X)", register_, *out);
-  e_warn();
+
+  if (e)
+    Log_Warn("%s", err_print_all(e));
+  else
+    Log_Debug("ReadByte(0x%X, out = 0x%X)", register_, *out);
+
   return e;
 }
 
 Error EC_Debug_ReadWord(uint8_t register_, uint16_t* out) {
   Error e = EC_Debug_Controller->ReadWord(register_, out);
-  Log_Debug("ReadWord(0x%X, out = 0x%X)", register_, *out);
-  e_warn();
+
+  if (e)
+    Log_Warn("%s", err_print_all(e));
+  else
+    Log_Debug("ReadWord(0x%X, out = 0x%X)", register_, *out);
+
   return e;
 }
 
