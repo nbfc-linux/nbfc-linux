ded1e667e3c575c55c7b240c6e3f92c9fa60df8d
Benjamin Abendroth <braph93@gmx.de>
2026-01-29 19:54:16 +0100

    nxjson_memory: Use a larger pool

--- src/nxjson_memory.h
+++ src/nxjson_memory.h
@@ -1,7 +1,7 @@
 #ifndef NXJSON_MEMORY_H_
 #define NXJSON_MEMORY_H_
 
-#define NXJSON_MEMORY_NUM_OBJECTS 256
+#define NXJSON_MEMORY_NUM_OBJECTS 512
 
 void* NXJSON_Memory_Calloc(size_t, size_t);
 void  NXJSON_Memory_Free(void*);
