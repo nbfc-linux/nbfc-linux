070f8d8e41b271ed93980bc655bf98bc0e881ce0
Benjamin Abendroth <braph93@gmx.de>
2026-02-10 15:47:29 +0100

    Use pid_t for process ids

--- src/client/service_control.c
+++ src/client/service_control.c
@@ -8,7 +8,6 @@
 #include <unistd.h> // unlink
 #include <limits.h> // INT_MAX
 
-#include <sys/types.h>
 #include <sys/socket.h> // connect, socket
 #include <sys/un.h>     // sockaddr_un
 
@@ -24,7 +23,7 @@
 #include "../service_config.h"
 #include "../file_utils.h"
 
-int Service_Get_PID() {
+pid_t Service_Get_PID() {
   const char* err;
   char buf[32];
   if (slurp_file(buf, sizeof(buf), NBFC_PID_FILE) == -1) {
@@ -39,7 +38,7 @@ int Service_Get_PID() {
   // trim the newline
   buf[strcspn(buf, "\n")] = '\0';
 
-  int pid = parse_number(buf, 0, INT_MAX, &err);
+  pid_t pid = parse_number(buf, 0, INT_MAX, &err);
   if (err) {
 error:
     Log_Error("Failed to read the pid file: " NBFC_PID_FILE ": %s", err);
@@ -182,7 +181,7 @@ void Service_LoadAllConfigFiles(ModelConfig* model_config) {
 }
 
 int Service_Start(bool read_only) {
-  int pid = Service_Get_PID();
+  pid_t pid = Service_Get_PID();
   if (pid != -1) {
     Log_Info("Service already running (pid: %d)", pid);
     return NBFC_EXIT_SUCCESS;
@@ -206,7 +205,7 @@ int Service_Start(bool read_only) {
 }
 
 int Service_Stop() {
-  int pid = Service_Get_PID();
+  pid_t pid = Service_Get_PID();
   if (pid == -1) {
     Log_Error("Service not running");
     return NBFC_EXIT_SUCCESS;
